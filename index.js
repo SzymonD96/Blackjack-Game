192e5286adeeba05e3f8b15b2d6f56289ae5d2c6
Szymon Duda <s.duda996@gmail.com>
2022-07-21 16:42:41 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let firstCard = 10;
+let secondCard = 7;
+let hasBlackjack = false;
+let sum = firstCard + secondCard;
+let isAlive = true;
+
+let = message = "";
+let messageEl = document.getElementById("message-el");
+// let sumEl = document.getElementById("sum-el");
+let sumEl = document.querySelector("#sum-el");
+
+function startGame() {
+
+    sumEl.textContent = "Sum: " + sum;
+
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?";
+    } else if (sum === 21) {
+        message = "You've got Blackjack!";
+        hasBlackjack = true;
+    } else {
+        message = "You're out of the game!";
+        isAlive = false;
+    }
+    messageEl.textContent = message;
+}
+
